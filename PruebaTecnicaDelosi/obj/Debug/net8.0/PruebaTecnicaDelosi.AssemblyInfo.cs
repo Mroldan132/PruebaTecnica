596fc3ad3c8be85426fc77042193b213a94bd0ef
Roldán Reynaga Martin Adriel <Compumart_@hotmail.com>
2025-02-26 22:31:21 -0500

    Docker compose agregado - Readme actualizado

--- PruebaTecnicaDelosi/obj/Debug/net8.0/PruebaTecnicaDelosi.AssemblyInfo.cs
+++ PruebaTecnicaDelosi/obj/Debug/net8.0/PruebaTecnicaDelosi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaTecnicaDelosi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4047e65ea3d000c25f5c10c7a74b298cb829d201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8a76f2f57140d59a7740152a0b273b3fd33aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaTecnicaDelosi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaTecnicaDelosi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
